97b655b6351e05b4b49936a680e1e2885e0449fd
shaoyahu <498486362@qq.com>
2022-09-13 23:32:26 +0800

    double

---
+++ src/pages/order/InfoModel.vue
@@ -0,0 +1,18 @@
+<template>
+  <el-drawer title="订单详情" v-model="dialogVisible" size="50%">
+
+  </el-drawer>
+</template>
+
+<script setup>
+import { ref, reactive } from 'vue'
+
+const dialogVisible = ref(false)
+
+const open = () =>{
+  dialogVisible.value = true
+}
+</script>
+
+<style lang='scss' scoped>
+</style>
